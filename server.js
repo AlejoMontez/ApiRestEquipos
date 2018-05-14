9ee3afa713005ee634f1645c9b07abf45fa58c52
Gabriel <alejo7mntz@gmail.com>
2018-05-14 14:55:00 -0500

    Cambio 4

--- server.js
+++ server.js
@@ -10,204 +10,204 @@ const PORT = process.env.PORT || 5000
 let teams = [
     {
         id: 1,
-        nombre: 'Atlanta Hawks',
+        nombret: 'Atlanta Hawks',
         logo: 'http://static.logomaker.com.s3-us-west-2.amazonaws.com/wp-content/uploads/2015/06/Atlanta-Hawks-PacMan-Logo.png',
-        entranador: 'Mike Budenholzer',
+        entrenador: 'Mike Budenholzer',
         estadio: [
-            {pabellon: 'Philips Arena', capacidad: '18.047', propiedad: 'Pública', año: '1999', ciudad: 'Atlanta',
+            {ide: 1, pabellon: 'Philips Arena', capacidad: '18.047', propiedad: 'Pública', anio: '1999', ciudad: 'Atlanta',
              url: 'http://nba.cdn.turner.com/nba/big/teams/hawks/2016/11/04/1478278120976-Incredible-Time-Lapse-Shows-7-Events-In-8-Days-at-Philips-Arena-915575-8.576x324.jpg '}
         ],
         jugadores: [
-            {id: 1, nombre: 'Malcolm Delaney'},
-            {id: 2, nombre: 'Jaylen Morris'},
-            {id: 3, nombre: 'John Collins'},
-            {id: 4, nombre: 'Miles Plumlee'},
-            {id: 5, nombre: 'Josh Magette'}
+            {idj: 1, nombrej: 'Malcolm Delaney'},
+            {idj: 2, nombrej: 'Jaylen Morris'},
+            {idj: 3, nombrej: 'John Collins'},
+            {idj: 4, nombrej: 'Miles Plumlee'},
+            {idj: 5, nombrej: 'Josh Magette'}
         ]
     },
     {
         id: 2,
-        nombre: 'Boston Celtics',
+        nombret: 'Boston Celtics',
         logo: 'http://ticketcrusader.com/wp-content/uploads/2015/09/Celtics.jpg',
-        entranador: 'Brad Stevens',
+        entrenador: 'Brad Stevens',
         estadio: [
-            {pabellon: 'TD Garden', capacidad: '18.624', propiedad: 'Privada', año: '1995', ciudad: 'Boston',
+            {ide: 1, pabellon: 'TD Garden', capacidad: '18.624', propiedad: 'Privada', anio: '1995', ciudad: 'Boston',
              url: 'http://www.destination360.com/north-america/us/massachusetts/images/s/td-garden.jpg'}
         ],
         jugadores: [
-            {id: 1, nombre: 'Terry Rozier'},
-            {id: 2, nombre: 'Abdel Nader'},
-            {id: 3, nombre: 'Marcus Morris'},
-            {id: 4, nombre: 'Greg Monroe'},
-            {id: 5, nombre: 'Kadeem Allen'}
+            {idj: 1, nombrej: 'Terry Rozier'},
+            {idj: 2, nombrej: 'Abdel Nader'},
+            {idj: 3, nombrej: 'Marcus Morris'},
+            {idj: 4, nombrej: 'Greg Monroe'},
+            {idj: 5, nombrej: 'Kadeem Allen'}
         ]
     },
     {
         id: 3,
-        nombre: 'Brooklyn Nets',
+        nombret: 'Brooklyn Nets',
         logo: 'http://a1.espncdn.com/combiner/i?img=%2Fi%2Fteamlogos%2Fnba%2F500%2Fbkn.png',
-        entranador: 'Kenny Atkinson',
+        entrenador: 'Kenny Atkinson',
         estadio: [
-            {pabellon: 'Barclays Center', capacidad: '17.732', propiedad: 'Pública', año: '2012', ciudad: 'Nueva York',
+            {ide: 1, pabellon: 'Barclays Center', capacidad: '17.732', propiedad: 'Pública', anio: '2012', ciudad: 'Nueva York',
              url: 'http://stadiumparkingguides.com/wp-content/uploads/2014/10/barclays_center_parking.jpg'}
         ],
         jugadores: [
-            {id: 1, nombre: 'Spencer Dinwiddie'},
-            {id: 2, nombre: 'Caris LeVert'},
-            {id: 3, nombre: 'Joe Harris'},
-            {id: 4, nombre: 'Dante Cunningham'},
-            {id: 5, nombre: 'Quincy Acy'}
+            {idj: 1, nombrej: 'Spencer Dinwiddie'},
+            {idj: 2, nombrej: 'Caris LeVert'},
+            {idj: 3, nombrej: 'Joe Harris'},
+            {idj: 4, nombrej: 'Dante Cunningham'},
+            {idj: 5, nombrej: 'Quincy Acy'}
         ]
     },{
         id: 4,
-        nombre: 'Charlotte Hornets',
+        nombret: 'Charlotte Hornets',
         logo: 'http://a1.espncdn.com/combiner/i?img=%2Fi%2Fteamlogos%2Fnba%2F500%2Fcha.png',
-        entranador: 'Steve Clifford',
+        entrenador: 'Steve Clifford',
         estadio: [
-            {pabellon: 'Spectrum Center', capacidad: '19.077', propiedad: 'Pública', año: '2005', ciudad: 'Charlotte',
+            {ide: 1, pabellon: 'Spectrum Center', capacidad: '19.077', propiedad: 'Pública', anio: '2005', ciudad: 'Charlotte',
              url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7cZ698LmrsmlsHx5CHkJGpJjGmAJrWtidWQHkg9DInQNhPKwgUQ'}
         ],
         jugadores: [
-            {id: 1, nombre: 'Kemba Walker'},
-            {id: 2, nombre: 'Nicolas Batum'},
-            {id: 3, nombre: 'Dwayne Bacon'},
-            {id: 4, nombre: 'Marvin Williams'},
-            {id: 5, nombre: 'Cody Zeller'}
+            {idj: 1, nombrej: 'Kemba Walker'},
+            {idj: 2, nombrej: 'Nicolas Batum'},
+            {idj: 3, nombrej: 'Dwayne Bacon'},
+            {idj: 4, nombrej: 'Marvin Williams'},
+            {idj: 5, nombrej: 'Cody Zeller'}
         ]
     },{
         id: 5,
-        nombre: 'Chicago Bulls',
+        nombret: 'Chicago Bulls',
         logo: 'http://ticketcrusader.com/wp-content/uploads/2015/09/Chicago-Bulls.jpg',
-        entranador: 'Fred Hoiberg',
+        entrenador: 'Fred Hoiberg',
         estadio: [
-            {pabellon: 'United Center', capacidad: '20.917', propiedad: 'Privada', año: '1994', ciudad: 'Chicago',
+            {ide: 1, pabellon: 'United Center', capacidad: '20.917', propiedad: 'Privada', anio: '1994', ciudad: 'Chicago',
              url: 'http://www.insidearenas.com/wp-content/uploads/2016/09/united2011.jpg'}
         ],
         jugadores: [
-            {id: 1, nombre: 'Kris Dunn'},
-            {id: 2, nombre: 'David Nwaba'},
-            {id: 3, nombre: 'Paul Zipser'},
-            {id: 4, nombre: 'Noah Vonleh'},
-            {id: 5, nombre: 'Cristiano Felicio'}
+            {idj: 1, nombrej: 'Kris Dunn'},
+            {idj: 2, nombrej: 'David Nwaba'},
+            {idj: 3, nombrej: 'Paul Zipser'},
+            {idj: 4, nombrej: 'Noah Vonleh'},
+            {idj: 5, nombrej: 'Cristiano Felicio'}
         ]
     },
     {
         id: 6,
-        nombre: 'Cleveland Cavaliers',
+        nombret: 'Cleveland Cavaliers',
         logo: 'http://www.logo-designer.co/wp-content/uploads/2017/07/2017-Cleveland-Cavaliers-logo-design.png',
-        entranador: 'Tyronn Lue',
+        entrenador: 'Tyronn Lue',
         estadio: [
-            {pabellon: 'Quicken Loans Arena', capacidad: '20.562', propiedad: 'Pública', año: '1994', ciudad: 'Cleveland',
+            {ide: 1, pabellon: 'Quicken Loans Arena', capacidad: '20.562', propiedad: 'Pública', anio: '1994', ciudad: 'Cleveland',
              url: 'http://crg.us.com/wp-content/uploads/2015/07/cavs-flames_1_mg_7184-final.jpg'}
         ],
         jugadores: [
-            {id: 1, nombre: 'George Hill'},
-            {id: 2, nombre: 'Rodney Hood'},
-            {id: 3, nombre: 'LeBron James'},
-            {id: 4, nombre: 'Okaro White'},
-            {id: 5, nombre: 'Tristan Thompsoncio'}
+            {idj: 1, nombrej: 'George Hill'},
+            {idj: 2, nombrej: 'Rodney Hood'},
+            {idj: 3, nombrej: 'LeBron James'},
+            {idj: 4, nombrej: 'Okaro White'},
+            {idj: 5, nombrej: 'Tristan Thompsoncio'}
         ]
     },
     {
         id: 7,
-        nombre: 'Dallas Mavericks',
+        nombret: 'Dallas Mavericks',
         logo: 'http://www.caratulasylogos.com/sites/default/files/styles/medium/public/escudos/dallas_mavericks.jpg?itok=50mQfAFA',
-        entranador: 'Michael Lue',
+        entrenador: 'Michael Lue',
         estadio: [
-            {pabellon: 'American Airlines Center', capacidad: '19.200', propiedad: 'Pública', año: '2001', ciudad: 'Dallas',
+            {ide: 1, pabellon: 'American Airlines Center', capacidad: '19.200', propiedad: 'Pública', anio: '2001', ciudad: 'Dallas',
              url: 'http://stadiumparkingguides.com/wp-content/uploads/2014/10/american_airlines_center_parking.jpg'}
         ],
         jugadores: [
-            {id: 1, nombre: 'Yogi Ferrell'},
-            {id: 2, nombre: 'Seth Curry'},
-            {id: 3, nombre: 'Harrison Barnes'},
-            {id: 4, nombre: 'Dwight Powell'},
-            {id: 5, nombre: 'Salah Mejri'}
+            {idj: 1, nombrej: 'Yogi Ferrell'},
+            {idj: 2, nombrej: 'Seth Curry'},
+            {idj: 3, nombrej: 'Harrison Barnes'},
+            {idj: 4, nombrej: 'Dwight Powell'},
+            {idj: 5, nombrej: 'Salah Mejri'}
         ]
     },
     {
         id: 8,
-        nombre: 'Denver Nuggets',
+        nombret: 'Denver Nuggets',
         logo: 'http://ava7.com/w/basketball-teams/denver-nuggets/denver-nuggets-nba-basketball-team.jpg',
-        entranador: 'Steve Curry',
+        entrenador: 'Steve Curry',
         estadio: [
-            {pabellon: 'Pepsi Center', capacidad: '19.155', propiedad: 'Privada', año: '1999', ciudad: 'Denver',
+            {ide: 1, pabellon: 'Pepsi Center', capacidad: '19.155', propiedad: 'Privada', anio: '1999', ciudad: 'Denver',
              url: 'http://emmashercliff.typepad.com/photos/uncategorized/2008/04/08/p3290095.jpg'}
         ],
         jugadores: [
-            {id: 1, nombre: 'Devin Harris'},
-            {id: 2, nombre: 'Gary Harris'},
-            {id: 3, nombre: 'Wilson Chandler'},
-            {id: 4, nombre: 'Paul Millsap'},
-            {id: 5, nombre: 'Mason Plumlee'}
+            {idj: 1, nombrej: 'Devin Harris'},
+            {idj: 2, nombrej: 'Gary Harris'},
+            {idj: 3, nombrej: 'Wilson Chandler'},
+            {idj: 4, nombrej: 'Paul Millsap'},
+            {idj: 5, nombrej: 'Mason Plumlee'}
         ]
     },
     {
         id: 9,
-        nombre: 'Detroit Pistons',
+        nombret: 'Detroit Pistons',
         logo: 'http://www.logo-designer.co/wp-content/uploads/2017/05/2017-detroit-pistons-logo-design.png',
-        entranador: 'Stan Van Gundy',
+        entrenador: 'Stan Van Gundy',
         estadio: [
-            {pabellon: 'Little Caesars Arena', capacidad: '20.491', propiedad: 'Pública', año: '2017', ciudad: 'Detroit',
+            {ide: 1, pabellon: 'Little Caesars Arena', capacidad: '20.491', propiedad: 'Pública', anio: '2017', ciudad: 'Detroit',
              url: 'http://lajugadafinanciera.com/wp-content/uploads/2016/11/Little-Caesars-Arena.jpg'}
         ],
         jugadores: [
-            {id: 1, nombre: 'Ish Smith'},
-            {id: 2, nombre: 'Luke Kennard'},
-            {id: 3, nombre: 'James Ennis'},
-            {id: 4, nombre: 'Anthony Tolliver'},
-            {id: 5, nombre: 'Eric Moreland'}
+            {idj: 1, nombrej: 'Ish Smith'},
+            {idj: 2, nombrej: 'Luke Kennard'},
+            {idj: 3, nombrej: 'James Ennis'},
+            {idj: 4, nombrej: 'Anthony Tolliver'},
+            {idj: 5, nombrej: 'Eric Moreland'}
         ]
     },
     {
         id: 10,
-        nombre: 'Golden State Warriors',
+        nombret: 'Golden State Warriors',
         logo: 'http://1.bp.blogspot.com/-p9rKsv3kPKM/VCgVOu5ayrI/AAAAAAAAFDc/niSaiuT940U/w1200-h630-p-k-no-nu/Logo%2BGolden%2BState%2BWarriors.png',
-        entranador: 'Steve Kerr',
+        entrenador: 'Steve Kerr',
         estadio: [
-            {pabellon: 'Oracle Arena', capacidad: '19.596', propiedad: 'Pública', año: '1966', ciudad: 'Oakland',
+            {ide: 1, pabellon: 'Oracle Arena', capacidad: '19.596', propiedad: 'Pública', anio: '1966', ciudad: 'Oakland',
              url: 'http://alphamechanicalservice.com/wp-content/uploads/2014/06/SanAntonio-1.jpg'}
         ],
         jugadores: [
-            {id: 1, nombre: 'Stephen Curry'},
-            {id: 2, nombre: 'Klay Thompson'},
-            {id: 3, nombre: 'Kevin Durant'},
-            {id: 4, nombre: 'Draymond Green'},
-            {id: 5, nombre: 'Damian Jones'}
+            {idj: 1, nombrej: 'Stephen Curry'},
+            {idj: 2, nombrej: 'Klay Thompson'},
+            {idj: 3, nombrej: 'Kevin Durant'},
+            {idj: 4, nombrej: 'Draymond Green'},
+            {idj: 5, nombrej: 'Damian Jones'}
         ]
     },
      {
         id: 11,
-        nombre: 'Houston Rockets',
+        nombret: 'Houston Rockets',
         logo: 'http://foxsportstexarkana.com/wp-content/uploads/2018/04/houston_rockets.png',
-        entranador: 'Mike Antoni',
+        entrenador: 'Mike Antoni',
         estadio: [
-            {pabellon: 'Toyota Center', capacidad: '18.055', propiedad: 'Pública', año: '2003', ciudad: 'Houston',
+            {ide: 1, pabellon: 'Toyota Center', capacidad: '18.055', propiedad: 'Pública', anio: '2003', ciudad: 'Houston',
              url: 'http://1c71hb3in51z3g8k1j1nogrdvsm.wpengine.netdna-cdn.com/wp-content/uploads/2017/11/1920px-Toyota_Center_inside.jpg'}
         ],
         jugadores: [
-            {id: 1, nombre: 'Eric Gordon'},
-            {id: 2, nombre: 'James Harden'},
-            {id: 3, nombre: 'Joe Johnson'},
-            {id: 4, nombre: 'Ryan Anderson'},
-            {id: 5, nombre: 'Zhou Qi'}
+            {idj: 1, nombrej: 'Eric Gordon'},
+            {idj: 2, nombrej: 'James Harden'},
+            {idj: 3, nombrej: 'Joe Johnson'},
+            {idj: 4, nombrej: 'Ryan Anderson'},
+            {idj: 5, nombrej: 'Zhou Qi'}
         ]
     },
      {
         id: 12,
-        nombre: 'Indiana Pacers',
+        nombret: 'Indiana Pacers',
         logo: 'http://thecourtsidecollective.com/wp-content/uploads/2012/10/indiana-pacers.gif',
-        entranador: 'Nate McMillan',
+        entrenador: 'Nate McMillan',
         estadio: [
-            {pabellon: 'Bankers Life Fieldhouse', capacidad: '18.165', propiedad: 'Pública', año: '1999', ciudad: 'Indiana',
+            {ide: 1, pabellon: 'Bankers Life Fieldhouse', capacidad: '18.165', propiedad: 'Pública', anio: '1999', ciudad: 'Indiana',
              url: 'http://www.ehc-global.com/wp-content/uploads/2016/09/Americas-USA-Bankers-Life-Fieldhouse-02.jpg'}
         ],
         jugadores: [
-            {id: 1, nombre: 'Cory Joseph'},
-            {id: 2, nombre: 'Victor Oladipo'},
-            {id: 3, nombre: 'Bojan Bogdanovic'},
-            {id: 4, nombre: 'Domantas Sabonis'},
-            {id: 5, nombre: 'Ben Moore'}
+            {idj: 1, nombrej: 'Cory Joseph'},
+            {idj: 2, nombrej: 'Victor Oladipo'},
+            {idj: 3, nombrej: 'Bojan Bogdanovic'},
+            {idj: 4, nombrej: 'Domantas Sabonis'},
+            {idj: 5, nombrej: 'Ben Moore'}
         ]
     }
     
@@ -235,8 +235,14 @@ app.get('/teams', (req, res) => {
 // http://127.0.0.1:5000/teams
 app.post('/teams', (req, res) => {
     let data = req.query;
-    teams.push(data.teams_name)
-    res.send("New team add")
+    let id = teams.length + 1;
+    let newTeam = {id: id, nombret: data.nombret, logo: data.logo, entrenador: data.entrenador, 
+        estadio: [{ide: data.ide, pabellon: data.pabellon, capacidad: data.capacidad, propiedad: data.propiedad, anio: data.anio, ciudad: data.ciudad}],
+        jugadores: [{idj: data.idj, nombrej: data.nombrej}]};
+    
+    teams.push(newTeam);
+    
+    res.send("New team add");
 })
 
 // URL para actualizar un usuario
@@ -244,7 +250,13 @@ app.post('/teams', (req, res) => {
 app.patch('/teams/:id',(req, res) => {
     let params = req.params;
     let data = req.query;
-    teams[params.id] = data
+    let id = parseInt(params.id) + 1;
+  
+    let newTeam = {id: id, nombret: data.nombret, logo: data.logo, entrenador: data.entrenador, 
+        estadio: [{ide: data.ide, pabellon: data.pabellon, capacidad: data.capacidad, propiedad: data.propiedad, anio: data.anio, ciudad: data.ciudad}],
+        jugadores: [{idj: data.idj, nombrej: data.nombrej}]};
+
+    teams[params.id] = newTeam
     res.send("Team update")
 })
 
